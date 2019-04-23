0e42dd601d1eb39c1cf1997fab172f419afd7c6e
rvwdude <rvwdude@gmail.com>
2019-04-23 14:24:10 +0300

    another commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,10 +4,6 @@ import '../styles/index.scss';
 
 
 new JustValidate('.form-a', {
-  
-  colorWrong: 'red',
-
-
   messages: {
     name: {
       minLength: 'My custom message about only minLength rule'
@@ -15,5 +11,16 @@ new JustValidate('.form-a', {
     email: 'My custom message about error (one error message for all rules)'
   },
 
-  
+  submitHandler: function (form, values, ajax) {
+
+    ajax({
+      url: 'https://just-validate-api.herokuapp.com/submit',
+      method: 'POST',
+      data: values,
+      async: true,
+      callback: function(response)  {
+        console.log(response);
+      }
+    });
+  },
 });
