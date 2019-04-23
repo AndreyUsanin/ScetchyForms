93c32134ce3303fd74aed2c381d5c613cbc91274
rvwdude <rvwdude@gmail.com>
2019-04-23 13:51:48 +0300

    shiit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,19 @@
 import '../styles/index.scss';
+
+
+
+
+new JustValidate('.form-a', {
+  
+  colorWrong: 'red',
+
+
+  messages: {
+    name: {
+      minLength: 'My custom message about only minLength rule'
+    },
+    email: 'My custom message about error (one error message for all rules)'
+  },
+
+  
+});
